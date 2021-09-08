b6cdc7c8e7087773a5744a9a2d9640e6c4e047f7
user <4sunskyhyun@gmail.com>
2021-09-08 22:08:56 +0900

    JPA API Regist Test code

---
+++ src/main/java/com/spring/test/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.spring.test.service.posts;
+
+import com.spring.test.domain.posts.PostsRepository;
+import com.spring.test.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
