0e97d81691d8e1e47c10e04a210e2206fe14088d
user <4sunskyhyun@gmail.com>
2021-09-09 21:05:14 +0900

    JPA 수정, 조회 API 추가

--- src/main/java/com/spring/test/service/posts/PostsService.java
+++ src/main/java/com/spring/test/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.spring.test.service.posts;
 
+import com.spring.test.domain.posts.Posts;
 import com.spring.test.domain.posts.PostsRepository;
+import com.spring.test.web.dto.PostsResponseDto;
 import com.spring.test.web.dto.PostsSaveRequestDto;
+import com.spring.test.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +19,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/spring/test/web/PostsApiController.java
+++ src/main/java/com/spring/test/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.spring.test.web;
 
 import com.spring.test.service.posts.PostsService;
+import com.spring.test.web.dto.PostsResponseDto;
 import com.spring.test.web.dto.PostsSaveRequestDto;
+import com.spring.test.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,8 +13,18 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")   /* 등록 API*/
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+    
+    @PutMapping("/api/v1/posts/{id}")   /* 수정 API*/
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+    
+    @GetMapping("/api/v1/posts/{id}")   /* 조회 API*/
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
