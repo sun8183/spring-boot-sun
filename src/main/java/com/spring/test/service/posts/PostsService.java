e1d830bf1576a137c3bc54093faf439dca856fc2
user <4sunskyhyun@gmail.com>
2021-11-02 22:04:35 +0900

    웹 게시판 등록 화면 css, js 구성

--- src/main/java/com/spring/test/service/posts/PostsService.java
+++ src/main/java/com/spring/test/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.spring.test.service.posts;
 
 import com.spring.test.domain.posts.Posts;
 import com.spring.test.domain.posts.PostsRepository;
+import com.spring.test.web.dto.PostsListResponseDto;
 import com.spring.test.web.dto.PostsResponseDto;
 import com.spring.test.web.dto.PostsSaveRequestDto;
 import com.spring.test.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -36,5 +40,11 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/spring/test/web/IndexController.java
+++ src/main/java/com/spring/test/web/IndexController.java
@@ -1,13 +1,23 @@
 package com.spring.test.web;
 
+import com.spring.test.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+    public IndexController(PostsService postsService) {
+        this.postsService = postsService;
+    }
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
 
