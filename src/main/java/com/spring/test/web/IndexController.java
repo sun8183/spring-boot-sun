74d7ca815919991a799dfe1bd1a6627400f70682
user <4sunskyhyun@gmail.com>
2021-11-08 22:18:15 +0900

    웹 게시판 수정 / 삭제 기능 추가

--- src/main/java/com/spring/test/web/IndexController.java
+++ src/main/java/com/spring/test/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.spring.test.web;
 
 import com.spring.test.service.posts.PostsService;
+import com.spring.test.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
@@ -24,5 +26,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
     }
 }
